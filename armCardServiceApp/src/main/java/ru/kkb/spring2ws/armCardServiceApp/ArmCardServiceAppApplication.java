95bdcd77759113b2b76b87235430bbdb0888b60b
alyanovao <89547364+alyanovao@users.noreply.github.com>
2021-10-14 22:50:17 +0300

    first commit

---
+++ armCardServiceApp/src/main/java/ru/kkb/spring2ws/armCardServiceApp/ArmCardServiceAppApplication.java
@@ -0,0 +1,13 @@
+package ru.kkb.spring2ws.armCardServiceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmCardServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmCardServiceAppApplication.class, args);
+	}
+
+}
---
+++ armCardServiceApp/src/test/java/ru/kkb/spring2ws/armCardServiceApp/ArmCardServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kkb.spring2ws.armCardServiceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmCardServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
